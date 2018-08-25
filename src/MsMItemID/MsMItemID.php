4ad6f65b2a78d4acca0c76d19dd3baa0cb5c92c0
MossMohs <mossmohs@gmail.com>
2018-08-25 19:15:43 +0900

    플레이어만 명령어를 사용할 수 있도록 수정

--- src/MsMItemID/MsMItemID.php
+++ src/MsMItemID/MsMItemID.php
@@ -5,6 +5,7 @@ namespace MsMItemID;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 
@@ -24,15 +25,16 @@ class MsMItemID extends PluginBase implements Listener {
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        switch(strtolower($command->getName())){
-            case "iv":
-                $player = $sender->getPlayer();
-                $item = $player->getInventory()->getItemInHand();
-                $player->sendMessage($item->getName() . " -> " . $item->getId() . ":" . $item->getDamage());
-                return true;
-            default:
-                return false;
+        if (!$sender instanceof Player) {
+            $sender->sendMessage(TextFormat::DARK_RED . "플레이어만 사용할 수 있는 명령어 입니다");
         }
+        if (strtolower($command->getName()) === "iv") {
+            $player = $sender->getPlayer();
+            $item = $player->getInventory()->getItemInHand();
+            $player->sendMessage($item->getName() . " -> " . $item->getId() . ":" . $item->getDamage());
+            return true;
+        }
+        return false;
     }
 }
 
